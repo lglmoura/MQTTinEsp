726fae9fef4ae7a4ecd2a9c3d9fad8f60f19c0da
Luiz Gustavo L Moura <lglmoura@gmail.com>
2019-05-15 23:54:50 -0300

    Criando sensor DHT11 com json

--- src/sensors/dht11.h
+++ src/sensors/dht11.h
@@ -1,5 +1,6 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <ArduinoJson.h>
 
 // construtor do objeto para comunicar com o sensor
 DHT dht(DHTPIN, DHTTYPE);
@@ -7,6 +8,9 @@ DHT dht(DHTPIN, DHTTYPE);
 //variáveis que armazenarão os valores lidos da umidade e temperatura
 float umidade = 0.0;
 float temperatura = 0.0;
+
+
+char* createJson(float temperatura, float umidade);
 
 boolean initSensorDHT11(void){
  
@@ -17,7 +21,7 @@ boolean initSensorDHT11(void){
 
 void readSensorDHT11() {
 
-  //faz a leitura da umidade
+   //faz a leitura da umidade
   umidade = dht.readHumidity();
    Serial.print("Umidade: ");
    Serial.println(umidade);
@@ -25,7 +29,25 @@ void readSensorDHT11() {
   temperatura = dht.readTemperature();
   Serial.print("temp: ");
   Serial.println(String(temperatura).c_str());
-  MQTTClient.publish(TOPICOTEM2, String(temperatura).c_str());
+  char* jsonTemHum = createJson(temperatura , umidade);
+  Serial.println(jsonTemHum);
+  MQTTClient.publish(TOPICOTEM2, jsonTemHum);
   MQTTClient.publish(TOPICOUMI2,String(umidade).c_str());
 
+  }
+
+
+  char* createJson(float temperatura, float umidade){
+    StaticJsonDocument<500> doc;
+    doc["sensor"] = "DHT11";
+    doc["typeSensor"]="TempHum";
+    doc["time"]="1234567890";
+    JsonArray values = doc.createNestedArray("data");
+    values.add(temperatura);
+    values.add(umidade);
+    char json[256];
+    serializeJson(doc, json);;
+    
+
+    return json;
   }

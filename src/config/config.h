ade22b805c9349faee048593eb4db047aef08c2c
Luiz Gustavo L Moura <lglmoura@gmail.com>
2019-05-15 22:16:52 -0300

    Criando arquivo de configuração

---
+++ src/config/config.h
@@ -0,0 +1,8 @@
+//defines
+#define SSID_REDE     "saeg2019"  //coloque aqui o nome da rede que se deseja conectar
+#define SENHA_REDE    "semsenha"  //coloque aqui a senha da rede que se deseja conectar
+
+#define MQTT_SERVER "iotsaeg.nc2.iff.edu.br"
+#define MQTT_USER   "saeg"
+#define MQTT_PASS   "semsenha"
+#define PORTA   1883
